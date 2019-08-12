6590b4cff54bc6bcf3c4ad551e13d03d1d6bd860
Hussam <husam610@gmail.com>
2019-08-12 12:57:38 +0200

    control of note page

---
+++ app/Http/Controllers/NoteCon.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Note;
+use App\Page;
+
+
+class NoteCon extends Controller
+{
+    public function recNote(Request $request, Page $page)
+   {
+     $validatedData = $request->validate([
+          'newNote' => 'required | min:3',
+          
+     ],[
+          'newNote.required' => ' الرجاء تعبئة الحقل اولاً ',
+          'newNote.min' => ' يرجى كتابة مالا يقل عن ٣ حروف '
+     ]);
+
+     $note =new Note;
+     $note-> text = $request->newNote;
+     $page->notes()->save($note);
+     return back();
+   }
+
+   public function delNote(Note $page){
+          $page->delete();
+          return back();
+   }
+
+   public function edit(Note $note){
+        return view('notes.edit',compact('note'));
+   }
+
+   public function update(Request $request, Note $note){
+        $note-> text = $request -> get('editNote');
+        $note->save();
+          //echo $note->text;
+        return redirect('table/'. $note->page_id);
+
+   }
+
+
+
+
+
+
+}
