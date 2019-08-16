b5b3ec737f5b1a6443abd93927953474563acdf7
Hussam <husam610@gmail.com>
2019-08-16 17:57:41 +0200

    add protected to 'title' field

--- app/Page.php
+++ app/Page.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
+    protected $fillable = ['title'];
+
     public function notes()
     {
         return $this->hasMany(Note::class);
